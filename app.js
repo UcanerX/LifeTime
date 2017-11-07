c0c455cc471df43613127d4fb0a122b17f15bf22
GitHub <noreply@github.com>
2017-11-08 01:05:29 +0800

    测试提交数据会不会乱码

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ App({
   window:null,
   onLaunch: function() {
     //调用API从本地缓存中获取数据
+    //测试github提交数据会不会乱码
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
